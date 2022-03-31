7c57257b66c89a02954d828d2152d5689c4af5db
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-31 23:39:05 +0600

    used useNavigate hook foe showing details

--- src/Components/Meal/Meal.js
+++ src/Components/Meal/Meal.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { Col } from "react-bootstrap";
 import "./Meal.css";
+import { useNavigate } from "react-router-dom";
 
 const Meal = ({ meal }) => {
   console.log(meal);
   const { idMeal, strMeal, strMealThumb } = meal;
+  const navigate = useNavigate();
+  const showDetails = () => {
+    const path = `/meal/${idMeal}`;
+    navigate(path);
+  };
 
   return (
     <Col>
-      <div className="meal-card">
+      <div className="meal-card" onClick={showDetails}>
         <img src={strMealThumb} className="img-fluid" alt="" />
         <h4 className="text-center py-2 fw-bold">{strMeal}</h4>
       </div>
---
+++ src/Components/MealDetails/MealDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MealDetails.css'
+
+const MealDetails = () => {
+      return (
+            <div>
+                  
+            </div>
+      );
+};
+
+export default MealDetails;
